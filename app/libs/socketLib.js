8f6f9ac28e5ee4fbf9d2e7220548cf5ea8167683
pankajsaini123 <pankajsaini982134@gmail.com>
2018-09-18 16:09:18 +0530

    complete group user functionality

--- app/libs/socketLib.js
+++ app/libs/socketLib.js
@@ -21,7 +21,7 @@ let setServer = (server) => {
 
     let allOnlineUsers = []
 
-    let groupOnlineRoomsAndUsers = {}
+    let listOfRoomAndNames = {}
 
     let io = socketio.listen(server);
 
@@ -53,46 +53,47 @@ let setServer = (server) => {
                    
                     socket.fullName = fullName
                     let userObj = { userId: currentUser.userId, fullName:fullName }
-                    allOnlineUsers.push(userObj)
-
-
-
-                    console.log(allOnlineUsers)
-                    myIo.emit('online-user-list', allOnlineUsers)
+                    allOnlineUsers.push(userObj)  
+                    
+                    console.log(Object.keys(listOfRoomAndNames))
+                    myIo.emit('allRooms', Object.keys(listOfRoomAndNames))
 
 
             // create new room
             socket.on('create-room', (roomName) => {
             console.log("roomName ============> " + roomName )
 
-             if (socket.room) {
+            if (socket.room != undefined) {
 
-            socket.emit('disconnect') 
+            socket.emit('disconnect')
             } 
        
 
             socket.room = roomName;
+            
 
-            if (groupOnlineRoomsAndUsers[socket.room] != undefined ) {
-                groupOnlineRoomsAndUsers[socket.room].push(userObj)
+            if (listOfRoomAndNames[socket.room] != undefined ) {
+                //listOfRoomAndNames[socket.room] = []
+                listOfRoomAndNames[socket.room].push(userObj)
 
             } else {
 
-            groupOnlineRoomsAndUsers[socket.room] = new Array()
-            groupOnlineRoomsAndUsers[socket.room].push(userObj)
+            listOfRoomAndNames[socket.room] = new Array()
+            listOfRoomAndNames[socket.room].push(userObj)
 
             }
 
-            console.log("group Online users")
-            console.log(groupOnlineRoomsAndUsers[socket.room] )
-            console.log('=====================')
-
+            console.log(listOfRoomAndNames)
 
             socket.join(socket.room)
-            socket.to(socket.room).broadcast.emit('group-online-users', groupOnlineRoomsAndUsers[socket.room])
 
-            console.log(Object.keys(groupOnlineRoomsAndUsers))
-            myIo.emit('allRooms', Object.keys(groupOnlineRoomsAndUsers))
+
+            console.log(listOfRoomAndNames[socket.room])
+            //socket.to(socket.room).broadcast.emit('group-online-users', listOfRoomAndNames[socket.room])
+            socket.to(socket.room).broadcast.emit('online-user-list', listOfRoomAndNames[socket.room])
+
+            console.log(Object.keys(listOfRoomAndNames))
+            myIo.emit('allRooms', Object.keys(listOfRoomAndNames))
             
 
         })
@@ -104,8 +105,8 @@ let setServer = (server) => {
 
         }) // end of listening set-user event
 
-        console.log(Object.keys(groupOnlineRoomsAndUsers))
-        myIo.emit('allRooms', Object.keys(groupOnlineRoomsAndUsers)) 
+       /*  console.log(Object.keys(listOfRoomAndNames))
+        myIo.emit('allRooms', Object.keys(listOfRoomAndNames))  */
 
 
        
@@ -118,67 +119,27 @@ let setServer = (server) => {
             //socket.emit('disconnect')
 
 
-            delete groupOnlineRoomsAndUsers[socket.room]
-            console.log(groupOnlineRoomsAndUsers)
+            delete listOfRoomAndNames[socket.room]
+            console.log(listOfRoomAndNames)
 
 
             socket.room = editedRoomData.newRoomName
 
             //delete this.groupOnlineUserList[editedRoomData.currentRoomName]
-            groupOnlineRoomsAndUsers[socket.room] = new Array()
-            groupOnlineRoomsAndUsers[socket.room].push(this.userObj)
+            listOfRoomAndNames[socket.room] = new Array()
+            listOfRoomAndNames[socket.room].push(this.userObj)
 
             console.log(socket.room)
             socket.join(socket.room)
-            socket.to(socket.room).broadcast.emit('group-online-users', groupOnlineRoomsAndUsers[socket.room])
+            socket.emit('online-user-list', listOfRoomAndNames[socket.room])
+            socket.to(socket.room).broadcast.emit('group-online-users', listOfRoomAndNames[socket.room])
         
-            console.log(Object.keys(groupOnlineRoomsAndUsers))
-            myIo.emit('allRooms', Object.keys(groupOnlineRoomsAndUsers))
+            console.log(Object.keys(listOfRoomAndNames))
+            myIo.emit('allRooms', Object.keys(listOfRoomAndNames))
         
             /* console.log("edited room name list")
-            console.log(this.groupOnlineRoomsAndUsers) */
+            console.log(this.listOfRoomAndNames) */
         })
-
-
-        socket.on('switch-room', (joinRoomName) => {
-            console.log(socket.room)
-
-            console.log(socket.fullName + "has left" + socket.room)
-
-
-            if (socket.room != undefined) {
-               // socket.emit('disconnect')
-               socket.leave(socket.room)
-            }
-
-            socket.room = joinRoomName
-            socket.join(socket.room)
-            console.log(socket.fullName + "has joined" + socket.room)
-
-
-            socket.to(socket.room).broadcast.emit('joinedRoom', socket.fullName)
-
-            let userObj = { userId: socket.userId, fullName: socket.fullName }
-            console.log(userObj)
-
-            if(groupOnlineRoomsAndUsers[socket.room] != undefined){
-                groupOnlineRoomsAndUsers[socket.room].push(userObj);
-            }
-            else{
-                groupOnlineRoomsAndUsers[socket.room] = new Array();
-                groupOnlineRoomsAndUsers[socket.room].push(userObj);
-            }
-
-            console.log(groupOnlineRoomsAndUsers[socket.room])
-            socket.to(socket.room).broadcast.emit('group-online-users', groupOnlineRoomsAndUsers[socket.room])
-
-            console.log(Object.keys(groupOnlineRoomsAndUsers))
-            myIo.emit('allRooms', Object.keys(groupOnlineRoomsAndUsers))
-
-        })
-
-
-        
 
 
         socket.on('disconnect', () => {
@@ -191,35 +152,98 @@ let setServer = (server) => {
             // console.log(socket.connectorName);
             console.log(socket.userId);
             //socket.emit('valueForJoin',0);
-            socket.to(socket.room).broadcast.emit('leftRoom',socket.fullName); 
+            socket.to(socket.room).broadcast.emit('leftRoom',socket.fullName);
+            console.log(socket.room +  '<================ socket.room') 
             
-            if ( socket.room) {
+            if ( socket.room != undefined) {
 
-           /*  var removeIndex = groupOnlineRoomsAndUsers[socket.room].map(function(user) { return user.userId }).indexOf(socket.userId);
-            groupOnlineRoomsAndUsers[socket.room].splice(removeIndex,1) */
+            var removeIndex = listOfRoomAndNames[socket.room].map(function(user) { return user.userId }).indexOf(socket.userId);
+            listOfRoomAndNames[socket.room].splice(removeIndex,1) 
 
-            //deleting room from array if no user exist in room
-             if(groupOnlineRoomsAndUsers[socket.room].length == 0)
-                delete groupOnlineRoomsAndUsers[socket.room]
-
-                socket.leave(socket.room)
- 
             }
+            //deleting room from array if no user exist in room
+             /* if(listOfRoomAndNames[socket.room].length == 0)
+                delete listOfRoomAndNames[socket.room] */
+
+               
+ 
+            
             //listing rooms and users
-            console.log(groupOnlineRoomsAndUsers)
+            console.log(listOfRoomAndNames)
 
             //listing room names
-            console.log(Object.keys(groupOnlineRoomsAndUsers));
-            /* var removeIndex = allOnlineUsers.map(function(user) { return user.userId; }).indexOf(socket.userId);
+            //console.log(Object.keys(listOfRoomAndNames));
+             var removeIndex = allOnlineUsers.map(function(user) { return user.userId; }).indexOf(socket.userId);
             allOnlineUsers.splice(removeIndex,1)
-            console.log(allOnlineUsers) */ 
+            console.log(allOnlineUsers) 
 
-            socket.to(socket.room).broadcast.emit('group-online-users',groupOnlineRoomsAndUsers[socket.room]);
-            //socket.broadcast.emit('allRooms',Object.keys(groupOnlineRoomsAndUsers))
-            //socket.leave(socket.room)
+            //socket.to(socket.room).broadcast.emit('group-online-users',listOfRoomAndNames[socket.room]);
+            //socket.broadcast.emit('allRooms',Object.keys(listOfRoomAndNames))
+            socket.emit('online-user-list', listOfRoomAndNames[socket.room])
+            socket.to(socket.room).broadcast.emit('online-user-list', listOfRoomAndNames[socket.room])
+            socket.leave(socket.room)
             //socket.disconnect()
 
         }) // end of on disconnect
+
+
+
+
+
+
+
+        socket.on('switch-room', (joinRoomName) => {
+            console.log(socket.room)
+
+            console.log(socket.fullName + "has left" + socket.room)
+
+
+             if (socket.room != undefined) {
+                socket.emit('disconnect')
+               //socket.leave(socket.room)
+            } 
+
+            socket.room = joinRoomName
+            socket.join(socket.room)
+            console.log(socket.fullName + " has joined " + socket.room)
+
+
+            socket.to(socket.room).broadcast.emit('joinedRoom', socket.fullName)
+
+            let userObj = { userId: socket.userId, fullName: socket.fullName }
+            //console.log(userObj + "<==================== userObj")
+
+            if(listOfRoomAndNames[socket.room] != undefined) {
+                
+                listOfRoomAndNames[socket.room].push(userObj);                    
+               
+            }
+            else{
+                listOfRoomAndNames[socket.room] = new Array();
+                listOfRoomAndNames[socket.room].push(userObj);
+            }
+            console.log("*********room details*********************************")
+            console.log(listOfRoomAndNames)
+            //console.log(`${listOfRoomAndNames[socket.room]} <=================== all users of  ${socket.room}  group`)
+            //console.log(listOfRoomAndNames[socket.room] + "<=================== all users of" + socket.room + "group")
+            //socket.to(socket.room).broadcast.emit('group-online-users', listOfRoomAndNames[socket.room])
+            //myIo.emit('group-online-users', listOfRoomAndNames[socket.room])
+            console.log("switch room online user list")
+            console.log(listOfRoomAndNames[socket.room])
+            //socket.to(socket.room).broadcast.emit('group-online-users', listOfRoomAndNames[socket.room])
+            socket.emit('online-user-list', listOfRoomAndNames[socket.room])
+            socket.to(socket.room).broadcast.emit('online-user-list', listOfRoomAndNames[socket.room])
+
+            console.log(Object.keys(listOfRoomAndNames))
+            myIo.emit('allRooms', Object.keys(listOfRoomAndNames))
+
+        })
+
+
+        
+
+
+        
 
 
         socket.on('chat-msg', (data) => {
